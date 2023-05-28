d507229705328aaf250cb891dd6d067c9612c45a
tomeczeklmaooo <tomeczeklmao@proton.me>
2023-05-28 23:51:30 +0200

    Update Starlight Preview

--- starlight-engine-design-preview/script.js
+++ starlight-engine-design-preview/script.js
@@ -1,3 +1,12 @@
+var global_vars = {};
+
+var engine_strings = {
+	slx_core: "Starlight",
+	slx_core_version: "design_preview_rev.2",
+	slx_core_architecture: "x86_64",
+	slx_core_operating_system: "Windows NT 10.0"
+};
+
 function Initialize()
 {
 	engine_footer();
@@ -5,9 +14,23 @@ function Initialize()
 	engine_response_warning();
 	engine_response_error();
 	engine_timestamp();
+	engine_design_preview_version();
 }
 
 function engine_footer()
 {
-	document.getElementById("engine_footer").innerHTML = `<pre>Built on slx_core slx_core_version (slx_core_architecture; slx_core_operating_system)</pre>`;
+	document.getElementById("engine_footer").innerHTML = `<pre>Built on ${engine_strings.slx_core} ${engine_strings.slx_core_version} (${engine_strings.slx_core_architecture}; ${engine_strings.slx_core_operating_system})</pre>`;
+}
+
+function engine_response_success() {}
+
+function engine_response_warning() {}
+
+function engine_response_error() {}
+
+function engine_timestamp() {}
+
+function engine_design_preview_version()
+{
+	document.getElementById("design-preview-revision").innerHTML = `rev. 2`;
 }
