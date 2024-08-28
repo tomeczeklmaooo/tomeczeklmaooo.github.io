6d156161feaac23190ec4f637ffeee8ce4bb7d72
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-08-28 20:55:55 +0200

    Update logging.js

--- scripts/logging.js
+++ scripts/logging.js
@@ -3,27 +3,27 @@ var color_green = "color: #00ff00;";
 var color_blue = "color: #6aaffd;";
 var color_yellow = "color: #ffff00;";
 var color_orange = "color: #ffa500;";
-var color_white = "color: #ffffff;";
+var color_normal = "color: inherit;";
 
 function lerror(message)
 {
-	console.log(`%cERROR\t>> %c${message}`, color_red, color_white);
+	console.log(`%cERROR\t>> %c${message}`, color_red, color_normal);
 }
 function lwarning(message)
 {
-	console.log(`%cWARN\t>> %c${message}`, color_orange, color_white);
+	console.log(`%cWARN\t>> %c${message}`, color_orange, color_normal);
 }
 function lsuccess(message)
 {
-	console.log(`%cSUCCESS\t>> %c${message}`, color_green, color_white);
+	console.log(`%cSUCCESS\t>> %c${message}`, color_green, color_normal);
 }
 function linfo(message)
 {
-	console.log(`%cINFO\t>> %c${message}`, color_blue, color_white);
+	console.log(`%cINFO\t>> %c${message}`, color_blue, color_normal);
 }
 function llog(message)
 {
-	console.log(`%cLOG\t\t>> %c${message}`, color_yellow, color_white);
+	console.log(`%cLOG\t\t>> %c${message}`, color_yellow, color_normal);
 }
 
 // lerror("Error message here.");
