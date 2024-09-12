d731eb1c758b7950fdab47dd88b42a2a3b363107
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-09-12 17:37:11 +0200

    begin game design

---
+++ sites/clicker/core.js
@@ -0,0 +1,29 @@
+var cvars = {
+	// GAMEPLAY
+	game: {
+		clicks: 0,
+		experience: 0,
+		bread_pieces: 0,
+		level: 1,
+		level_multiplier: 1.2, // how much the required XP will go up with each level up
+		powerup_limit: 10,
+		autoclick: 0,
+		autoclick_delay: 2
+	},
+	// GFX
+	gfx: {
+		prettymode: 1, // enable or disable animations and effects
+		theme: 'themes.default' // change the theme
+	},
+	// SFX
+	audio: {
+		pack: 'audio.default',
+		master_volume: 1,
+		sfx_volume: 1,
+		music_volume: 1
+	},
+	// ANTI-CHEAT
+	anticheat: {
+		enabled: 1
+	}
+}
