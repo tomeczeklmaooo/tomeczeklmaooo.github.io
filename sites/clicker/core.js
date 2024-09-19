c2cb9b58e272ff4b916dcb190bb8dca5c4a559dd
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-09-19 19:15:49 +0200

    better way of printing cvars and replace img

--- sites/clicker/core.js
+++ sites/clicker/core.js
@@ -40,31 +40,8 @@ function clkPrintCvars(visible)
 {
 	if (visible) document.getElementById('cvars').style.display = 'block';
 
-	document.getElementById("cvars").innerHTML  = `Game Configuration Variables:<br>`;
-	document.getElementById("cvars").innerHTML += `Clicks: ${cvars.game.clicks}<br>`;
-	document.getElementById("cvars").innerHTML += `Experience: ${cvars.game.experience}<br>`;
-	document.getElementById("cvars").innerHTML += `Bread Pieces: ${cvars.game.bread_pieces}<br>`;
-	document.getElementById("cvars").innerHTML += `Level: ${cvars.game.level}<br>`;
-	document.getElementById("cvars").innerHTML += `Level Multiplier: ${cvars.game.level_multiplier}<br>`;
-	document.getElementById("cvars").innerHTML += `Powerup Limit: ${cvars.game.powerup_limit}<br>`;
-	document.getElementById("cvars").innerHTML += `Autoclick: ${cvars.game.autoclick}<br>`;
-	document.getElementById("cvars").innerHTML += `Autoclick Delay: ${cvars.game.autoclick_delay}<br>`;
-	
-	document.getElementById("cvars").innerHTML += `<br>Graphics Configuration Variables:<br>`;
-	document.getElementById("cvars").innerHTML += `Pretty Mode: ${cvars.gfx.prettymode}<br>`;
-	document.getElementById("cvars").innerHTML += `Theme: ${cvars.gfx.theme}<br>`;
-
-	document.getElementById("cvars").innerHTML += `<br>Audio Configuration Variables:<br>`;
-	document.getElementById("cvars").innerHTML += `Sound Pack: ${cvars.audio.sound_pack}<br>`;
-	document.getElementById("cvars").innerHTML += `Master Volume: ${cvars.audio.master_volume}<br>`;
-	document.getElementById("cvars").innerHTML += `SFX Volume: ${cvars.audio.sfx_volume}<br>`;
-	document.getElementById("cvars").innerHTML += `Music Volume: ${cvars.audio.music_volume}<br>`;
-
-	document.getElementById("cvars").innerHTML += `<br>Anti-Cheat Configuration Variables:<br>`;
-	document.getElementById("cvars").innerHTML += `Enabled: ${cvars.anticheat.enabled}<br>`;
-
-	document.getElementById("cvars").innerHTML += `<br>Misc Configuration Variables:<br>`;
-	document.getElementById("cvars").innerHTML += `Timeout: ${cvars.timeout}<br>`;
+	document.getElementById("cvars").innerHTML = ``;
+	document.getElementById("cvars").innerHTML += JSON.stringify(cvars, null, 4).replaceAll('"', '').replaceAll(',', '');
 
 	setTimeout(clkPrintCvars, cvars.timeout); // refresh every .1 seconds
 }
