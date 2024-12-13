a2c342095d9787c8ea0cbd647b6e41371e6b94c9
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-12-13 18:53:45 +0100

    fetch more data, begin media modal prep

--- sites/esp-dashboard/script.js
+++ sites/esp-dashboard/script.js
@@ -6,7 +6,7 @@ function bitRead(value, bit)
 }
 
 // THINGSPEAK FIELDS
-var g_ts_response;
+var g_ts_response, g_ts_media_response_a, g_ts_media_response_b;
 var wartosci;
 var enea_import;
 var temp_zew_bmp, temp_zew_vilant, wilg_zew;
@@ -34,142 +34,151 @@ var pompa_grzej, pompa_podloga, pompa_pieca, grzalka_cwu, pompa_cwu, pompa_sciek
 
 function thingspeak_fields()
 {
-	fetch('https://api.thingspeak.com/channels/432818/status.json?results=1').then((res) => res.text()).then((response) => {
-		g_ts_response = JSON.parse(response);
+	fetch('https://api.thingspeak.com/channels/432818/status.json?results=1').then((res) => res.text()).then((main_response) => {
+		fetch('https://api.thingspeak.com/channels/864444/feeds.json?results=480').then((res) => res.text()).then((media_response_a) => {
+			fetch('https://api.thingspeak.com/channels/432818/fields/4.json?results=240').then((res) => res.text()).then((media_response_b) => {
+				g_ts_response = JSON.parse(main_response);
+				g_ts_media_response_a = JSON.parse(media_response_a);
+				g_ts_media_response_b = JSON.parse(media_response_b);
 
-		wartosci = g_ts_response['feeds'][0]['status'].split('|');
+				console.log(g_ts_media_response_a);
+				console.log(g_ts_media_response_b);
 
-		enea_import            = wartosci[0];
-		temp_zew_bmp           = wartosci[1];
-		temp_zew_vilant        = wartosci[2];
-		wilg_zew               = wartosci[3];
-		opady_dzis             = wartosci[4];
-		cisnienie              = wartosci[5];
-		temp_salon_bmp         = wartosci[6];
-		temp_salon_vilant      = wartosci[7];
-		temp_pietro            = wartosci[8];
-		temp_garaz             = wartosci[9];
-		temp_strych_garaz      = wartosci[10];
-		temp_strych            = wartosci[11];
-		temp_lazienka          = wartosci[12];
-		wilg_wew               = wartosci[13];
-		stan_pieca             = wartosci[14];
-		cis_wody               = wartosci[15];
-		gaz_co                 = wartosci[16];
-		gaz_cwu                = wartosci[17];
-		temp_cwu               = wartosci[18];
-		t_set_podloga          = wartosci[19];
-		t_zas_podloga          = wartosci[20];
-		t_set_grzejniki        = wartosci[21];
-		t_zas_grzejniki        = wartosci[22];
-		ilosc_woda_dom         = wartosci[23];
-		ilosc_woda_ogrod       = wartosci[24];
-		pv_produkcja_dzis      = wartosci[25];
-		pv_konsumpcja_dzis     = wartosci[26];
-		pv_autokonsumpcja_dzis = wartosci[27];
-		pv_akt_obciazenie      = wartosci[28];
-		pv_power               = wartosci[29];
-		pv_napiecie            = wartosci[30];
-		pv_cena_sprzedazy      = wartosci[31];
-		pv_cena_zakupu         = wartosci[32];
-		zakodowany_boolean     = wartosci[33];
-		sunrise                = wartosci[34];
-		sunset                 = wartosci[35];
+				wartosci = g_ts_response['feeds'][0]['status'].split('|');
 
-		drzwi_gosp             = bitRead(zakodowany_boolean, 0);
-		brama_wjazd            = bitRead(zakodowany_boolean, 1);
-		brama_garaz            = bitRead(zakodowany_boolean, 2);
-		okno_salon             = bitRead(zakodowany_boolean, 3);
-		smart_plug             = bitRead(zakodowany_boolean, 4);
-		zawor_wod_dom          = bitRead(zakodowany_boolean, 5);
-		empty_1                = bitRead(zakodowany_boolean, 6);
-		empty_2                = bitRead(zakodowany_boolean, 7);
-		swiatlo_lazienka       = bitRead(zakodowany_boolean, 8);
-		swiatlo_garaz          = bitRead(zakodowany_boolean, 9);
-		swiatlo_pom_gosp       = bitRead(zakodowany_boolean, 10);
-		swiatlo_strych         = bitRead(zakodowany_boolean, 11);
-		pralka                 = bitRead(zakodowany_boolean, 12);
-		zmywarka               = bitRead(zakodowany_boolean, 13);
-		fox_online             = bitRead(zakodowany_boolean, 14);
-		drzwi_wej              = bitRead(zakodowany_boolean, 15);
-		min_zbiornik           = bitRead(zakodowany_boolean, 16);
-		med_zbiornik           = bitRead(zakodowany_boolean, 17);
-		max_zbiornik           = bitRead(zakodowany_boolean, 18);
-		min_studnia            = bitRead(zakodowany_boolean, 19);
-		max_studnia            = bitRead(zakodowany_boolean, 20);
-		p_studnia_zbiornik     = bitRead(zakodowany_boolean, 21);
-		max_scieki             = bitRead(zakodowany_boolean, 22);
-		zmiekczacz             = bitRead(zakodowany_boolean, 23);
-		pompa_grzej            = bitRead(zakodowany_boolean, 24);
-		pompa_podloga          = bitRead(zakodowany_boolean, 25);
-		pompa_pieca            = bitRead(zakodowany_boolean, 26);
-		grzalka_cwu            = bitRead(zakodowany_boolean, 27);
-		pompa_cwu              = bitRead(zakodowany_boolean, 28);
-		pompa_scieki           = bitRead(zakodowany_boolean, 29);
-		pompa_studnia          = bitRead(zakodowany_boolean, 30);
-		pompa_zbiornik         = bitRead(zakodowany_boolean, 31);
+				enea_import            = wartosci[0];
+				temp_zew_bmp           = wartosci[1];
+				temp_zew_vilant        = wartosci[2];
+				wilg_zew               = wartosci[3];
+				opady_dzis             = wartosci[4];
+				cisnienie              = wartosci[5];
+				temp_salon_bmp         = wartosci[6];
+				temp_salon_vilant      = wartosci[7];
+				temp_pietro            = wartosci[8];
+				temp_garaz             = wartosci[9];
+				temp_strych_garaz      = wartosci[10];
+				temp_strych            = wartosci[11];
+				temp_lazienka          = wartosci[12];
+				wilg_wew               = wartosci[13];
+				stan_pieca             = wartosci[14];
+				cis_wody               = wartosci[15];
+				gaz_co                 = wartosci[16];
+				gaz_cwu                = wartosci[17];
+				temp_cwu               = wartosci[18];
+				t_set_podloga          = wartosci[19];
+				t_zas_podloga          = wartosci[20];
+				t_set_grzejniki        = wartosci[21];
+				t_zas_grzejniki        = wartosci[22];
+				ilosc_woda_dom         = wartosci[23];
+				ilosc_woda_ogrod       = wartosci[24];
+				pv_produkcja_dzis      = wartosci[25];
+				pv_konsumpcja_dzis     = wartosci[26];
+				pv_autokonsumpcja_dzis = wartosci[27];
+				pv_akt_obciazenie      = wartosci[28];
+				pv_power               = wartosci[29];
+				pv_napiecie            = wartosci[30];
+				pv_cena_sprzedazy      = wartosci[31];
+				pv_cena_zakupu         = wartosci[32];
+				zakodowany_boolean     = wartosci[33];
+				sunrise                = wartosci[34];
+				sunset                 = wartosci[35];
 
-		// switch do stanu pieca
-		switch (parseInt(stan_pieca))
-		{
-			case 0:
-				piec_status = "Ogrzewanie: Brak zapotrzebowania";
-				break;
-			case 1:
-				piec_status = "Ogrzewanie: Rozruch wentylator";
-				break;
-			case 2:
-				piec_status = "Ogrzewanie: Praca pompy";
-				break;
-			case 3:
-				piec_status = "Ogrzewanie: Zapłon";
-				break;
-			case 4:
-				piec_status = "Ogrzewanie: Palnik włączony";
-				break;
-			case 5:
-				piec_status = "Ogrzewanie: Wybieg";
-				break;
-			case 6:
-				piec_status = "Ogrzewanie: Wybieg wentylatora";
-				break;
-			case 7:
-				piec_status = "Ogrzewanie: Wybieg pompy";
-				break;
-			case 8:
-				piec_status = "Ogrzewanie: Blokada palnika";
-				break;
-			case 20:
-				piec_status = "CWU: Zapotrzebowanie";
-				break;
-			case 21:
-				piec_status = "CWU: Rozruch wentylator";
-				break;
-			case 22:
-				piec_status = "CWU: Praca pompy";
-				break;
-			case 23:
-				piec_status = "CWU: Zapłon";
-				break;
-			case 24:
-				piec_status = "CWU: Palnik włączony";
-				break;
-			case 25:
-				piec_status = "CWU: Wybieg";
-				break;
-			case 26:
-				piec_status = "CWU: Wybieg wentylatora";
-				break;
-			case 27:
-				piec_status = "CWU: Wybieg pompy";
-				break;
-			case 28:
-				piec_status = "CWU: Blokada palnika";
-				break;
-		}
+				drzwi_gosp             = bitRead(zakodowany_boolean, 0);
+				brama_wjazd            = bitRead(zakodowany_boolean, 1);
+				brama_garaz            = bitRead(zakodowany_boolean, 2);
+				okno_salon             = bitRead(zakodowany_boolean, 3);
+				smart_plug             = bitRead(zakodowany_boolean, 4);
+				zawor_wod_dom          = bitRead(zakodowany_boolean, 5);
+				empty_1                = bitRead(zakodowany_boolean, 6);
+				empty_2                = bitRead(zakodowany_boolean, 7);
+				swiatlo_lazienka       = bitRead(zakodowany_boolean, 8);
+				swiatlo_garaz          = bitRead(zakodowany_boolean, 9);
+				swiatlo_pom_gosp       = bitRead(zakodowany_boolean, 10);
+				swiatlo_strych         = bitRead(zakodowany_boolean, 11);
+				pralka                 = bitRead(zakodowany_boolean, 12);
+				zmywarka               = bitRead(zakodowany_boolean, 13);
+				fox_online             = bitRead(zakodowany_boolean, 14);
+				drzwi_wej              = bitRead(zakodowany_boolean, 15);
+				min_zbiornik           = bitRead(zakodowany_boolean, 16);
+				med_zbiornik           = bitRead(zakodowany_boolean, 17);
+				max_zbiornik           = bitRead(zakodowany_boolean, 18);
+				min_studnia            = bitRead(zakodowany_boolean, 19);
+				max_studnia            = bitRead(zakodowany_boolean, 20);
+				p_studnia_zbiornik     = bitRead(zakodowany_boolean, 21);
+				max_scieki             = bitRead(zakodowany_boolean, 22);
+				zmiekczacz             = bitRead(zakodowany_boolean, 23);
+				pompa_grzej            = bitRead(zakodowany_boolean, 24);
+				pompa_podloga          = bitRead(zakodowany_boolean, 25);
+				pompa_pieca            = bitRead(zakodowany_boolean, 26);
+				grzalka_cwu            = bitRead(zakodowany_boolean, 27);
+				pompa_cwu              = bitRead(zakodowany_boolean, 28);
+				pompa_scieki           = bitRead(zakodowany_boolean, 29);
+				pompa_studnia          = bitRead(zakodowany_boolean, 30);
+				pompa_zbiornik         = bitRead(zakodowany_boolean, 31);
 
-		// odwloania funkcji ktore potrzebuja tych zmiennych ponizej
-		generate_squares();
+				// switch do stanu pieca
+				switch (parseInt(stan_pieca))
+				{
+					case 0:
+						piec_status = "Ogrzewanie: Brak zapotrzebowania";
+						break;
+					case 1:
+						piec_status = "Ogrzewanie: Rozruch wentylator";
+						break;
+					case 2:
+						piec_status = "Ogrzewanie: Praca pompy";
+						break;
+					case 3:
+						piec_status = "Ogrzewanie: Zapłon";
+						break;
+					case 4:
+						piec_status = "Ogrzewanie: Palnik włączony";
+						break;
+					case 5:
+						piec_status = "Ogrzewanie: Wybieg";
+						break;
+					case 6:
+						piec_status = "Ogrzewanie: Wybieg wentylatora";
+						break;
+					case 7:
+						piec_status = "Ogrzewanie: Wybieg pompy";
+						break;
+					case 8:
+						piec_status = "Ogrzewanie: Blokada palnika";
+						break;
+					case 20:
+						piec_status = "CWU: Zapotrzebowanie";
+						break;
+					case 21:
+						piec_status = "CWU: Rozruch wentylator";
+						break;
+					case 22:
+						piec_status = "CWU: Praca pompy";
+						break;
+					case 23:
+						piec_status = "CWU: Zapłon";
+						break;
+					case 24:
+						piec_status = "CWU: Palnik włączony";
+						break;
+					case 25:
+						piec_status = "CWU: Wybieg";
+						break;
+					case 26:
+						piec_status = "CWU: Wybieg wentylatora";
+						break;
+					case 27:
+						piec_status = "CWU: Wybieg pompy";
+						break;
+					case 28:
+						piec_status = "CWU: Blokada palnika";
+						break;
+				}
+
+				// odwloania funkcji ktore potrzebuja tych zmiennych ponizej
+				generate_squares();
+			}).catch((e) => console.error(e));
+		}).catch((e) => console.error(e));
 	}).catch((e) => console.error(e));
 }
 
@@ -188,7 +197,7 @@ function generate_squares()
 	var buf = "";
 	// === KARTY ===
 	var card_pogoda = `
-			<div class="card card-2x1" onclick="show_chart()">
+			<div class="card card-2x1" onclick="show_chart(0)">
 				<div class="card-header">
 					<span><i class="fa-solid fa-cloud-sun-rain"></i> Pogoda</span>
 				</div>
@@ -502,7 +511,7 @@ function generate_squares()
 		</div>
 	`;
 	var card_media = `
-		<div class="card card-1x1">
+		<div class="card card-1x1" onclick="show_chart(1)">
 			<div class="card-header">
 				<span><i class="fa-solid fa-industry"></i> Media</span>
 			</div>
@@ -682,14 +691,48 @@ function generate_squares()
 	buf += `</div>`;
 	buf += `</div>`;
 	
-	buf += `<div id="modal" class="chart-modal">`;
+	buf += `<div id="modal-weather" class="chart-modal">`;
 	buf += `<div class="chart-modal-content">`;
 	buf += `<span>Wykres temperatury</span>`;
 	buf += `<span class="close">&times;</span>`;
 	buf += `<iframe width="100%" height="400" style="border: 0;" src="https://thingspeak.mathworks.com/channels/432818/charts/2?dynamic=true&results=240&export=true&width=auto&height=auto"></iframe>`;
 	buf += `</div>`;
 	buf += `</div>`;
+
+	buf += `<div id="modal-media" class="chart-modal">`;
+	buf += `<div class="chart-modal-content">`;
+	buf += `<span>Wykresy mediów</span>`;
+	buf += `<span class="close">&times;</span>`;
+	buf += `<div id="media_chart_container></div>`;
+	buf += `</div>`;
+	buf += `</div>`;
 	document.getElementById('main').innerHTML += buf;
+
+	// HIGHCHARTS SETUP
+	// const chart_today = Highcharts.chart('media_chart_container', {
+	// 	chart: { type: 'line' },
+	// 	title: { text: undefined },
+	// 	xAxis: { categories: [] },
+	// 	yAxis: { title: { text: undefined }, labels: { enabled: false } },
+	// 	plotOptions: {
+	// 		line: { dataLabels: { enabled: true }, enableMouseTracking: true },
+	// 		series: { animation: { duration: 1200 } }
+	// 	},
+	// 	tooltip: { shared: true, crosshairs: true },
+	// 	series: [
+	// 		{ name: 'Sprzedaż', data: [], animation: { defer: 1200 } },
+	// 		{ name: 'Zakup', data: [], animation: { defer: 2500 }, color: "#ff2014", dashStyle: 'dash' }
+	// 	],
+	// 	accessibility: { enabled: false },
+	// 	credits: { enabled: false }
+	// });
+
+	// var x_axis_categories = [
+	// 	'00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+	// 	'06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
+	// 	'12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+	// 	'18:00', '19:00', '20:00', '21:00', '22:00', '23:00'
+	// ];
 }
 
 function remote_control()
@@ -719,18 +762,39 @@ function remote_control()
 	document.getElementById('main').innerHTML += buf;
 }
 
-function show_chart()
+function show_chart(_type)
 {
-	var modal = document.getElementById('modal');
+	var modal_weather = document.getElementById('modal-weather');
+	var modal_media = document.getElementById('modal-media');
 	var close = document.getElementsByClassName('close')[0];
-	modal.style.visibility = 'visible';
-	close.onclick = function()
+	switch (_type)
 	{
-		modal.style.visibility = 'hidden';
-	}
-	window.onclick = function(event)
-	{
-		if (event.target == modal) modal.style.visibility = 'hidden';
+		case 0:
+		{
+			modal_weather.style.visibility = 'visible';
+			close.onclick = function()
+			{
+				modal_weather.style.visibility = 'hidden';
+			}
+			window.onclick = function(event)
+			{
+				if (event.target == modal_weather) modal_weather.style.visibility = 'hidden';
+			}
+			break;
+		}
+		case 1:
+		{
+			modal_media.style.visibility = 'visible';
+			close.onclick = function()
+			{
+				modal_media.style.visibility = 'hidden';
+			}
+			window.onclick = function(event)
+			{
+				if (event.target == modal_media) modal_media.style.visibility = 'hidden';
+			}
+			break;
+		}
 	}
 }
 
