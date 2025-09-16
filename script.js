e22b658c6695bf545808f81dab9d0fa578a31e91
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-09-16 22:50:25 +0200

    new more retro website (reject modernity)

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// nothing here
+function get_date()
+{
+	const date = new Date();
+	const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const months = [
+		'January', 'February', 'March', 'April',
+		'May', 'June', 'July', 'August',
+		'September', 'October', 'November', 'December'
+	];
+
+	document.querySelector('.date').innerHTML = `Today is: ${weekdays[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
